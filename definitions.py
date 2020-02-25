bdc8944c595ead6be5aea294cf9258003228d1d1
Krystian Nikiel <krystiankontakt15@gmail.com>
2020-02-25 15:44:35 +0100

    Added basic network watcher

--- definitions.py
+++ definitions.py
@@ -1,4 +1,6 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_STORAGE_DIR = os.path.join(ROOT_DIR, 'data_storage')
+
+NETWORK_TIMER_INTERVAL = 10
