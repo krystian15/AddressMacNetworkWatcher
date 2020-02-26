9755231848d4227142d888a90a5e0b1f362cec14
Krystian Nikiel <krystiankontakt15@gmail.com>
2020-02-26 15:05:56 +0100

    Added intagrate to API

---
+++ mattermost/mattermost_api.py
@@ -0,0 +1,27 @@
+import requests
+from definitions import BOT_TOKEN, API_URL
+
+
+class MattermostRESTAPI:
+
+    def __init__(self):
+        self.chanel_id = 'd7odg6aizpnoiyh81e74ehz8me'
+        self.headers = {
+            'content-type': 'application/json',
+            'Accept-Charset': 'UTF-8',
+            'authorization': f'Bearer {BOT_TOKEN}'
+        }
+
+    def send_message(self, message: str):
+        payload = {
+            "channel_id": self.chanel_id,
+            "message": message
+        }
+        requests.post(f'{API_URL}/posts', json=payload, headers=self.headers)
+
+    def clear_channel(self):
+        pass
+
+
+if __name__ == "__main__":
+    MattermostRESTAPI()
--- network_watcher/watcher.py
+++ network_watcher/watcher.py
@@ -3,12 +3,14 @@ from definitions import NETWORK_TIMER_INTERVAL
 from mac_addresses.address_mac_generator import AddressMacGenerator
 from network_watcher.date_helper import is_new_day, get_current_date_timestamp
 from network_watcher.hosts_helper import get_new_offline_users, get_new_online_users
+from mattermost.mattermost_api import MattermostRESTAPI
 
 
 class NetworkWatcher:
 
     def __init__(self):
-        self.online_hosts = []
+        self.api = MattermostRESTAPI()
+        self.online_hosts = AddressMacGenerator().addresses
         self.offline_hosts = []
         self.today_timestamp = get_current_date_timestamp()
 
@@ -22,19 +24,27 @@ class NetworkWatcher:
 
         online_hosts = AddressMacGenerator().addresses
 
-        print(f'Online {get_new_online_users(self.online_hosts, online_hosts)}')
-        print(f'Offline {get_new_offline_users(online_hosts, self.online_hosts)}')
+        new_online_hosts = get_new_online_users(self.online_hosts, online_hosts)
+        new_offline_users = get_new_offline_users(online_hosts, self.online_hosts)
+
+        if new_online_hosts:
+            self.new_online_users_update(new_online_hosts)
+
+        if new_offline_users:
+            self.new_offline_users_update(new_offline_users)
 
         self.online_hosts = online_hosts
 
     def new_day_update(self):
         pass
 
-    def new_online_users_update(self):
-        pass
+    def new_online_users_update(self, hosts):
+        for host in hosts:
+            self.api.send_message(message=f"New online user {host}")
 
-    def new_offline_users_update(self):
-        pass
+    def new_offline_users_update(self, hosts):
+        for host in hosts:
+            self.api.send_message(message=f"New offline user {host}")
 
 
 NetworkWatcher()
